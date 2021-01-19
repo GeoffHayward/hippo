09807f4a9445dd1335d8ddfec08c57be81a7bfbe
Rafał Kociński <rafal.kocinski@infinityworks.com>
2021-01-19 16:35:41 +0000

    [ADZ-343] Balance of headings

---
+++ cms/src/test/java/uk/nhs/digital/apispecs/commonmark/HeadingLevelOffsetterTest.java
@@ -0,0 +1,20 @@
+package uk.nhs.digital.apispecs.commonmark;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class HeadingLevelOffsetterTest {
+
+    @Test
+    public void name() {
+        fail("Test not implemented, yet");
+
+        // given
+
+        // when
+
+        // then
+
+    }
+}
